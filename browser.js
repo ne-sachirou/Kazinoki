598f4887549060b52461aa11d12efe26067477db
ne_Sachirou <utakata.c4se@gmail.com>
2015-11-02 07:36:22 +0900

    React with ES6 class.

--- browser.js
+++ browser.js
@@ -1,12 +1,12 @@
 var remote = require('remote');
 var kazinoki = remote.require('./kazinoki').instance;
 
-var App = React.createClass({
-  render: function () {
+class App extends React.Component {
+  render() {
     return (
       <p>{kazinoki.config.booksDir}</p>
     );
-  },
-});
+  }
+};
 
 ReactDOM.render(<App/>, document.getElementById('main'));
